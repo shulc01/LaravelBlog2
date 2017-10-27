da312fcb034e1a84d183e13d2b941ee0a02b7640
shulc01 <agroruno@gmail.com>
2017-10-27 16:29:29 +0300

    final check

--- database/migrations/2017_10_12_121927_create_articles_table.php
+++ database/migrations/2017_10_12_121927_create_articles_table.php
@@ -26,7 +26,6 @@ class CreateArticlesTable extends Migration
             
             $table->timestamps();
 
-             //$table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
 
 
--- database/migrations/2017_10_27_132805_create_images_table.php
+++ database/migrations/2017_10_27_132805_create_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoriesTable extends Migration
+class CreateImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,9 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::enableForeignKeyConstraints();
-
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name', 45);
-            $table->text('description');
-            $table->integer('parent_id');
-            $table->timestamps();
-
+            $table->string('name', 255);
         });
     }
 
@@ -32,6 +26,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('images');
     }
 }
