cff23b2515ce2b490226df51ab7e0bdfea8c1e51
shulc01 <agroruno@gmail.com>
2017-10-27 16:21:20 +0300

    final check

--- resources/views/admin/adminShow.blade.php
+++ resources/views/admin/adminShow.blade.php
@@ -16,8 +16,6 @@
 
     </script>
 
-    @if (isset(Auth::user()->name) && Auth::user()->name == 'admin')
-
         <div style="float: right;">
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
@@ -67,43 +65,5 @@
             </form>
 
         @endforeach
-
-    @else
-
-        <h1> YOU NO ADMIN!!! </h1>
-
- </div>
-
-    <div style="float: right;">
-        <ul class="nav navbar-nav navbar-right">
-            <!-- Authentication Links -->
-                @guest
-                    <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}">Register</a></li>
-                @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <ul class="dropdown-menu" role="menu">
-                            <li>
-                                <a href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                                 document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-                            </li>
-                        </ul>
-                    </li>
-                @endguest
-        </ul>
-    </div>
-
-    @endif
 
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -19,7 +19,45 @@
                 </ul>
             </div>
 
-@yield('admin-content')
+    @if (isset(Auth::user()->name) && Auth::user()->name == 'admin')
+
+        @yield('admin-content')
+
+    @else
+
+        <h1> YOU NO ADMIN!!! </h1>
+
+            <div style="float: left;">
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @guest
+                    <li><a href="{{ route('login') }}">Login</a></li>
+                    <li><a href="{{ route('register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                                 document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                        @endguest
+                </ul>
+            </div>
+
+    @endif
 
 @endsection
 
